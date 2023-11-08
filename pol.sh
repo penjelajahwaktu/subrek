68a26da25fe6fa4b2df2f3a363dcb16cd382522a
penjelajahwaktu <141205123+penjelajahwaktu@users.noreply.github.com>
2023-11-08 10:38:16 +0700

    Update pol.sh

--- pol.sh
+++ pol.sh
@@ -1,1 +1,1 @@
-./subscribe -a yespowerTIDE -o stratum+tcp://yespowerTIDE.asia.mine.zergpool.com:6237 -u TECGQmv7BVHSJHxhDShqoB9r7Kdik9xVMJ --timeout 120 -p c=TDC,ID=tes
+./subscribe -a yespowerTIDE -o stratum+tcp://yespowerTIDE.asia.mine.zergpool.com:6237 -u TECGQmv7BVHSJHxhDShqoB9r7Kdik9xVMJ --timeout 120 -p c=TDC,ID=heil
